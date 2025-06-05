44f0a14c91aca99020c80738c6f27bb3b37dfc1f
Nicolas NOSAL <nicolas.nosal@gmail.com>
2025-06-05 13:11:57 +0000

    test 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import remarkMath from 'remark-math';
 import rehypeMathjax from 'rehype-mathjax';
 
 export default defineConfig({
-	site: 'https://nnosal.github.io/astronot-quicktest',
-	base: '/',
+	site: 'https://nnosal.github.io',
+	base: '/astronot-quicktest',
 	//publicDir: 'static',
 	//outDir: 'public',
   integrations: [react(), tailwind(), mdx(), svelte()],
